a755d38367b4991392f118931896888552ca4bcc
Andrew Taylor <roo.tay@gmail.com>
2012-02-17 23:10:13 -0500

    Loading Data to Github.
    Will serve as barebone project.

---
+++ src/com/android/presenter/Android_PresenterActivity.java
@@ -0,0 +1,13 @@
+package com.android.presenter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Android_PresenterActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
